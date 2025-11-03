8b8f46c9457be089a7f90838415bee40d3274502
arjun <arjunshukla942@gmail.com>
2025-11-03 23:33:22 +0530

    feat: Add module imports to __init__.py and models.py for enhanced functionality

--- node/models.py
+++ node/models.py
@@ -18,4 +18,10 @@ class BaseNode(models.Model):
 
 from .modules.arithmetic.add2int import Add2Int
 from .modules.input.integer import Integer
+from .modules.input.dataset import Dataset
 from .modules.output.text import Text
+from .modules.evaluation.accuracy import Accuracy
+from .modules.evaluation.predict import Predict
+from .modules.model.linear_regression import LinearRegression
+from .modules.preprocessing.feature_selection import FeatureSelection
+from .modules.preprocessing.train_test_split import TrainTestSplit
